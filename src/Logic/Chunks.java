03a11e30bca850773dbf00d15b31b45f3063747d
DianaP5 <digpinto@gmail.com>
2016-03-22 18:32:35 +0000

    update-chunks

--- src/Logic/Chunks.java
+++ src/Logic/Chunks.java
@@ -5,4 +5,31 @@ public class Chunks {
 	private static int number;
 	private final int maxLenght=64;
 	private static int size;
+	
+	private int ReplicationDeg;
+	
+	
+    private byte[] chunkNo;
+
+    public Chunks(int id, byte[] chunkNo) {
+        this.id = id;
+        this.chunkNo = chunkNo;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public int getChunkSize() {
+        return chunkNo.length;
+    }
+
+    public byte[] getChunkNo() {
+        return chunkNo;
+    }
+    
+    
+    public int getReplicationDeg(){
+    	return ReplicationDeg;
+    }
 }
